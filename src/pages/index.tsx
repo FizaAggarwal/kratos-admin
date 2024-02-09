e3a95f8e4246c6edb9cc4c2e9d0105712f1b5aeb
Fiza Aggarwal <fiza.aggarwal@gmail.com>
2024-02-09 15:09:36 +0530

    table UI for the list of identities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,28 @@ export default function Home({ data }: ListProps) {
   console.log(data, "###identities");
 
   return (
-    <div>
-      <div>Get identities</div>
+    <div className="bg-gray-900 text-white min-h-screen">
+      <div className="text-2xl font-bold py-5 px-8">Get identities</div>
+      <table className="table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+          <tr className="text-sm font-medium text-white uppercase bg-gray-800">
+            <th className="px-8 py-5">First Name</th>
+            <th className="px-8 py-5">Last Name</th>
+            <th className="px-8 py-5">Email</th>
+            <th className="px-8 py-5">ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((identity) => (
+            <tr key={identity.id} className="text-white bg-gray-700">
+              <td className="px-8 py-5">{identity.traits.name.first || "-"}</td>
+              <td className="px-8 py-5">{identity.traits.name.last || "-"}</td>
+              <td className="px-8 py-5">{identity.traits.email}</td>
+              <td className="px-8 py-5">{identity.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
